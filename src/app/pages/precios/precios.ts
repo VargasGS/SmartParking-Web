4e743a1b689001bcd9ceab33637f31fda81df189
VargasGS <sofivargas0310@gmail.com>
2026-04-07 21:08:29 -0600

    Se agrega logo y se termina la pantalla de pantenimiento de precios y tipo vehículo

--- src/app/pages/precios/precios.ts
+++ src/app/pages/precios/precios.ts
@@ -8,8 +8,9 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import {MatTableModule} from '@angular/material/table';
-
+import {MatTableModule,MatTableDataSource} from '@angular/material/table';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import { ViewChild, AfterViewInit } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
 
@@ -24,19 +25,73 @@ import { CommonModule } from '@angular/common';
     MatIconModule,
     MatCardModule,
     CommonModule,
-    MatTableModule],
+    MatTableModule,
+    MatPaginatorModule],
   templateUrl: './precios.html',
   styleUrl: './precios.css',
 })
-export class PreciosComponent {
+export class PreciosComponent implements AfterViewInit {
 
-  
-   form;
+  form;
+
+  precios: any[] = [
+  { tipoVehiculo: 'carro', precioC: 2, precioD: 1000 },
+  { tipoVehiculo: 'moto', precioC: 1, precioD: 500 }
+];
+   editIndex: number | null = null;
+
+ //* displayedColumns: string[] = ['IdPago', 'TipoVehiculo', 'Precio'];
+
+  dataSource = new MatTableDataSource([
+    { IdPago: 1, TipoVehiculo: 'Carro', Precio: 1200},
+    { IdPago: 2, TipoVehiculo: 'Moto', Precio: 300},
+    { IdPago: 3, TipoVehiculo: 'Bicicleta', Precio: 15 },
+    { IdPago: 4, TipoVehiculo: 'Bus', Precio: 5000 },
+  ]);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
       tipoVehiculo: ['', Validators.required],
-      precio: ['', [Validators.required, Validators.minLength(5)]],
+      precioC: ['', [Validators.required, Validators.minLength(5)]],
+      precioD: ['', [Validators.required, Validators.minLength(5)]],
     });
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  //Métodos CRUD
+
+   InsertTipoVehiculoPrecio() {
+    if (this.form.valid) {
+
+      const nuevo = {
+        tipoVehiculo: this.form.value.tipoVehiculo,
+        precioC: this.form.value.precioC,
+        precioD: this.form.value.precioD
+
+      };
+
+      this.precios.push(nuevo);
+
+      this.form.reset();
+    }
+  }
+
+   DeleteTipoVehiculoPrecio(index: number) {
+    this.precios.splice(index, 1);
+  }
+
+   UpdateTipoVehiculoPrecio(index: number, precioC:any, precioD:any) {
+    const colones = precioC.value;
+    const dolares = precioD.value;
+
+  if (!colones || !dolares) return;
+
+  this.precios[index].precioC = colones;
+  this.precios[index].precioD = dolares;
   }
 }
